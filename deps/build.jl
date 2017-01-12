6953173e4828817af712b14b5f162f41bb25c4fb
Robert Feldt <robert.feldt@gmail.com>
2017-01-12 16:15:25 +0100

    bug fix

--- deps/build.jl
+++ deps/build.jl
@@ -5,11 +5,13 @@ vers = "2.0" # Use version number in setup.py file of pyEMD: https://raw.githubu
 
 tagfile = "installed_vers"
 target = "libemd.$(Libdl.dlext)"
-url = "https://bintray.com/artifact/download/julialang/generic/"
+srcdir = joinpath(dirname(@__FILE__()), "c_src")
+
 if !isfile(target) || !isfile(tagfile) || readchomp(tagfile) != "$vers $(Sys.WORD_SIZE)"
-    cd("c_sec") do
+    cd(srcdir) do
         println("Compiling libemd...")
         run(`make -f make.emd LIB=../$target`)
+        run(`make -f make.emd clean`)
     end
     open(tagfile, "w") do f
         println(f, "$vers $(Sys.WORD_SIZE)")
